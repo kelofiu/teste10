72ed161bf3df620f0993869d704986646b39fb08
kelofiu <nicolasgaleote68@gmail.com>
2024-10-17 20:38:22 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -14,10 +14,10 @@ Configurar_Pagina("Exemplo 1",
 st.title("não é o primeito mais é o decimo")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("teste10")
+st.header("fesfe10")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("teste mais do que 10")
+st.subheader("fesfe mais do que 10")
 
 # Use st.write("") para adicionar um texto ao seu Web app
 st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
